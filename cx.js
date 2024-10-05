ec57bd3d04295dfe3595b5427a0cc48d239d5f45
Stevan <140596498+StevanGriliano@users.noreply.github.com>
2024-10-05 10:52:49 +0200

    Update cx.js

--- cx.js
+++ cx.js
@@ -2,3 +2,34 @@
   let num = 100;
 </script>
 
+"
+localName
+: 
+"body"
+namespaceURI
+: 
+"http://www.w3.org/1999/xhtml"
+nextElementSibling
+: 
+rq-implicit-test-rule-widget.rq-element
+nextSibling
+: 
+rq-implicit-test-rule-widget.rq-element
+nodeName
+: 
+"BODY"
+nodeType
+: 
+1
+nonce
+: 
+""
+offsetHeight
+: 
+759
+offsetLeft
+: 
+0
+offsetTop
+: 
+0
